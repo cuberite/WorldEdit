c5a08515d0afd4e047adcf49e56ef986047485b7
STRWarrior <niels.breuker@hotmail.nl>
2013-12-26 13:51:55 -0800

    Using ipairs instead of pairs.

--- API/Check.lua
+++ API/Check.lua
@@ -1,5 +1,5 @@
 function CheckIfInsideAreas(a_MinX, a_MaxX, a_MinY, a_MaxY, a_MinZ, a_MaxZ, a_Player, a_World, a_Operation)
-	for Key, Value in pairs(ExclusionArea[a_World:GetName()]) do
+	for Key, Value in ipairs(ExclusionArea[a_World:GetName()]) do
 		if Value.Plugin:Call(Value.FunctionName, a_MinX, a_MaxX, a_MinY, a_MaxY, a_MinZ, a_MaxZ, a_Player, a_World, a_Operation) then
 			return true
 		end
