9127ab8897ddac75a41fb19bfea133f93f69a025
madmaxoft <github@xoft.cz>
2013-12-29 20:07:41 +0100

    Renamed Commands.lua to std name Info.lua.

--- Info.lua
+++ Info.lua
@@ -1,10 +1,15 @@
+
+-- Info.lua
+
+-- Implements the g_PluginInfo standard plugin description
+
 g_PluginInfo = 
 {
 	Name = "WorldEdit",
 	Version = "0.1",
 	Date = "2013-12-29",
 	Description = [[
-		This plugin allows you to easily manage the world, edit the world, navigate around or get information.
+		This plugin allows you to easily manage the world, edit the world, navigate around or get information. It bears similarity to the Bukkit's WorldEdit plugin and aims to have the same set of commands,however, it has no affiliation to that plugin.
 	]],
 	Commands =
 	{
