417a4886285d0b521a5b5d678b39b7f2ebd218ef
madmaxoft <github@xoft.cz>
2013-12-29 21:05:43 +0100

    Added category builder in InfoDump.
    The dumper does nothing yet, but the category builder should work.

--- InfoDump.lua
+++ InfoDump.lua
@@ -7,8 +7,44 @@
 
 
 
---- 
-local function BuildCategoryCommands(a_CategoryName)
+--- Returns an array-table of all commands that are in the specified category
+-- Each item is a table {Command = "/command string", Info = {<command info in PluginInfo>}}
+local function GetCategoryCommands(a_CategoryName)
+	local res = {};
+	local function AppendCategoryCommand(a_Prefix, a_Commands)
+		for cmd, info in pairs(a_Commands) do
+			info.Category = info.Category or {};
+			if (type(info.Category) == "string") then
+				info.Category = {info.Category};
+			end
+			for idx, cat in ipairs(info.Category) do
+				if (cat == a_CategoryName) then
+					table.insert(res, {Command = a_Prefix .. cmd, Info = info});
+				end
+			end
+			if (info.Subcommands ~= nil) then
+				AppendCategory(a_Prefix .. cmd .. " ", info.Subcommands);
+			end
+		end
+	end
+	AppendCategoryCommand("", g_PluginInfo.Commands);
+	return res;
+end
+
+
+
+
+
+--- Builds the g_PluginInfo.Categories' Commands arrays, if not already present.
+-- The Commands array in each value is the table build by GetCategoryCommands()
+local function BuildCategories()
+	g_PluginInfo.Categories = g_PluginInfo.Categories or {};
+	for name, desc in pairs(g_PluginInfo.Categories) do
+		if (desc.Commands == nil) then
+			-- The Commands array has not been built yet, calculate it now:
+			desc.Commands = GetCategoryCommands(name);
+		end
+	end
 end
 
 
@@ -16,4 +52,23 @@ end
 
 
 function DumpPluginInfoForum()
+	-- Make sure the categories have their command lists:
+	BuildCategories();
+	
+	-- TODO
 end
+
+
+
+
+
+function DumpPluginInfoGitHub()
+	-- Make sure the categories have their command lists:
+	BuildCategories();
+	
+	-- TODO
+end
+
+
+
+
