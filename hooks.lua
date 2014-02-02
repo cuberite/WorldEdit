ba85a2388aa4d640d3ec4cc55acf614100343dab
STRWarrior <niels.breuker@hotmail.nl>
2014-02-02 13:38:50 +0100

    Cleanup in the Hooks code.

--- hooks.lua
+++ hooks.lua
@@ -2,17 +2,27 @@
 -----------------SELECTFIRSTPOINT------------------
 ---------------------------------------------------
 function SelectFirstPointHook(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
-	if (WandActivated[Player:GetName()]) then
-		if Player:GetEquippedItem().m_ItemType == Wand then
-			OnePlayer[Player:GetName()] = Vector3i(BlockX, BlockY, BlockZ)
-			if OnePlayer[Player:GetName()] ~= nil and TwoPlayer[Player:GetName()] ~= nil then
-				Player:SendMessage(cChatColor.LightPurple .. 'First position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0) (" .. GetSize(Player) .. ").")
-			else
-				Player:SendMessage(cChatColor.LightPurple .. 'First position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0).")
-			end
-			return true
-		end
+	if not PlayerHasWEPermission(PLayer, "worldedit.selection.pos") then
+		return false
 	end
+	
+	
+	local PlayerName = Player:GetName()
+	if not WandActivated[Player:GetName()] then
+		return false
+	end
+	
+	if Player:GetEquippedItem().m_ItemType ~= Wand then
+		return false
+	end
+	
+	OnePlayer[Player:GetName()] = Vector3i(BlockX, BlockY, BlockZ)
+	if OnePlayer[Player:GetName()] ~= nil and TwoPlayer[Player:GetName()] ~= nil then
+		Player:SendMessage(cChatColor.LightPurple .. 'First position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0) (" .. GetSize(Player) .. ").")
+	else
+		Player:SendMessage(cChatColor.LightPurple .. 'First position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0).")
+	end
+	return true
 end
 
 
@@ -32,7 +42,7 @@ end
 ---------------------------------------------------
 function LeftClickCompassHook(Player, BlockX, BlockY, BlockZ, BlockFace, Status)
 	if Status == 0 then
-		if Player:HasPermission("worldedit.navigation.jumpto.tool") and Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS then
+		if PlayerHasWEPermission(Player, "worldedit.navigation.jumpto.tool") and Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS then
 			LeftClickCompassUsed[Player:GetName()] = false
 			return false
 		end
@@ -44,23 +54,25 @@ end
 ---------------------TOOLSHOOK----------------------
 ----------------------------------------------------
 function ToolsHook(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
-	if (BlockX ~= -1) or (BlockY ~= 255) or (BlockZ ~= -1) then
-		local PlayerName = Player:GetName()
-		local World = Player:GetWorld()
-		if Player:GetEquippedItem().m_ItemType == ReplItem[PlayerName] then
-			Block = StringSplit(Repl[PlayerName], ":")
-			if Block[2] == nil then
-				Block[2] = 0
-			end
-			World:SetBlock(BlockX, BlockY, BlockZ, Block[1], Block[2])
-			return false
+	if BlockFace == BLOCK_FACE_NONE then
+		return false
+	end
+	
+	local PlayerName = Player:GetName()
+	local World = Player:GetWorld()
+	if Player:GetEquippedItem().m_ItemType == ReplItem[PlayerName] then
+		Block = StringSplit(Repl[PlayerName], ":")
+		if Block[2] == nil then
+			Block[2] = 0
 		end
-		if Player:GetEquippedItem().m_ItemType == GrowTreeItem[PlayerName] then
-			if World:GetBlock(BlockX, BlockY, BlockZ) == 2 or World:GetBlock(BlockX, BlockY, BlockZ) == 3 then
-				World:GrowTree(BlockX, BlockY + 1, BlockZ)
-			else
-				Player:SendMessage(cChatColor.Rose .. "A tree can't go there.")
-			end
+		World:SetBlock(BlockX, BlockY, BlockZ, Block[1], Block[2])
+		return false
+	end
+	if Player:GetEquippedItem().m_ItemType == GrowTreeItem[PlayerName] then
+		if World:GetBlock(BlockX, BlockY, BlockZ) == 2 or World:GetBlock(BlockX, BlockY, BlockZ) == 3 then
+			World:GrowTree(BlockX, BlockY + 1, BlockZ)
+		else
+			Player:SendMessage(cChatColor.Rose .. "A tree can't go there.")
 		end
 	end
 end
@@ -70,20 +82,30 @@ end
 -----------------SELECTSECONDPOINT------------------
 ----------------------------------------------------
 function SelectSecondPointHook(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
-	if (BlockX ~= -1) or (BlockY ~= 255) or (BlockZ ~= -1) then
-		if (WandActivated[Player:GetName()]) then
-			-- Check if the wand is equipped
-			if Player:GetEquippedItem().m_ItemType == Wand then
-				TwoPlayer[Player:GetName()] = Vector3i(BlockX, BlockY, BlockZ)
-				if OnePlayer[Player:GetName()] ~= nil and TwoPlayer[Player:GetName()] ~= nil then
-					Player:SendMessage(cChatColor.LightPurple .. 'Second position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0) (" .. GetSize(Player) .. ").")
-				else
-					Player:SendMessage(cChatColor.LightPurple .. 'Second position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0).")
-				end
-				return false
-			end
-		end
+	if BlockFace == BLOCK_FACE_NONE then
+		return false
 	end
+	
+	local PlayerName = Player:GetName()
+	if not PlayerHasWEPermission(Player, "worldedit.selection.pos") then
+		return false
+	end
+	
+	if not WandActivated[PlayerName] then
+		return false
+	end
+		-- Check if the wand is equipped
+	if Player:GetEquippedItem().m_ItemType ~= Wand then
+		return false
+	end
+	
+	TwoPlayer[PlayerName] = Vector3i(BlockX, BlockY, BlockZ)
+	if OnePlayer[Player:GetName()] ~= nil and TwoPlayer[Player:GetName()] ~= nil then
+		Player:SendMessage(cChatColor.LightPurple .. 'Second position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0) (" .. GetSize(Player) .. ").")
+	else
+		Player:SendMessage(cChatColor.LightPurple .. 'Second position set to (' .. BlockX .. ".0, " .. BlockY .. ".0, " .. BlockZ .. ".0).")
+	end
+	return true
 end
 
 
@@ -95,7 +117,7 @@ function RightClickCompassHook(Player, BlockX, BlockY, BlockZ, BlockFace, Cursor
 		return false
 	end
 	-- Check if the equipped item is a compass.
-	if (Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS) and Player:HasPermission("worldedit.navigation.thru.tool") then
+	if (Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS) and PlayerHasWEPermission(Player, "worldedit.navigation.thru.tool") then
 		RightClickCompass(Player, Player:GetWorld())
 	end
 end
@@ -114,15 +136,17 @@ end
 -----------------ONPLAYERANIMATION------------------
 ----------------------------------------------------
 function OnPlayerAnimation(Player, Animation)
-	if Animation == 1 then
-		local PlayerName = Player:GetName()
-		if LeftClickCompassUsed[PlayerName] or LeftClickCompassUsed[PlayerName] == nil then
-			if Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS and Player:HasPermission("worldedit.navigation.jumpto.tool") then
-				if not LeftClickCompass(Player, Player:GetWorld()) then
-					Player:SendMessage(cChatColor.Rose .. "No blocks in sight (or too far)!")
-				end
+	if Animation ~= 1 then
+		return false
+	end
+	
+	local PlayerName = Player:GetName()
+	if LeftClickCompassUsed[PlayerName] or LeftClickCompassUsed[PlayerName] == nil then
+		if Player:GetEquippedItem().m_ItemType == E_ITEM_COMPASS and PlayerHasWEPermission(Player, "worldedit.navigation.jumpto.tool") then
+			if not LeftClickCompass(Player, Player:GetWorld()) then
+				Player:SendMessage(cChatColor.Rose .. "No blocks in sight (or too far)!")
 			end
 		end
-		LeftClickCompassUsed[PlayerName] = true
 	end
+	LeftClickCompassUsed[PlayerName] = true
 end
