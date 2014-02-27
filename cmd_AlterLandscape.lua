78ebec363b01b683de2ae7c7f215f0e822e58210
madmaxoft <github@xoft.cz>
2014-02-27 10:05:52 +0100

    //drain and //sphere commands use the new architecture.

--- cmd_AlterLandscape.lua
+++ cmd_AlterLandscape.lua
@@ -94,39 +94,54 @@ end
 -----------------------------------------------
 ---------------------DRAIN---------------------
 -----------------------------------------------
-function HandleDrainCommand(Split, Player)
-	if tonumber(Split[2]) == nil or Split[2] == nil then -- check if the player gave a radius
-		Player:SendMessage(cChatColor.Rose .. "Too few arguments.\n//drain <radius>")
-		return true
-	else
-		Radius = tonumber(Split[2]) -- set the radius to the given radius
-	end
-	
-	local MinX = math.floor(Player:GetPosX()) - Radius
-	local MinY = math.floor(Player:GetPosY()) - Radius
-	local MinZ = math.floor(Player:GetPosZ()) - Radius
-	local MaxX = math.floor(Player:GetPosX()) + Radius
-	local MaxY = math.floor(Player:GetPosY()) + Radius
-	local MaxZ = math.floor(Player:GetPosZ()) + Radius
-	
-	if CheckIfInsideAreas(MinX, MaxX, MinY, MaxY, MinZ, MaxZ, Player, Player:GetWorld(), "drain") then
+function HandleDrainCommand(a_Split, a_Player)
+	-- Check the radius parameter:
+	local Radius = tonumber(a_Split[2])
+	if (Radius == nil) then
+		a_Player:SendMessage(cChatColor.Rose .. "Usage: //drain <radius>")
 		return true
 	end
 	
+	-- Check if other plugins allow the operation:
+	local X = math.floor(a_Player:GetPosX())
+	local Y = math.floor(a_Player:GetPosY())
+	local Z = math.floor(a_Player:GetPosZ())
+	local Cuboid = cCuboid(X - Radius, Y - Radius, Z - Radius, X + Radius, Y + Radius, Z + Radius)
+	Cuboid:ClampY(0, 255)
+	if not(CheckAreaCallbacks(Cuboid, a_Player, a_Player:GetWorld(), "drain")) then
+		return true
+	end
+	
+	-- TODO: Do a proper areafill algorithm (BFS / DFS) to replace only the connected bodies of fluid,
+	-- instead of replacing everything in the area
+	
+	-- Push the area to Undo stack:
+	local State = GetPlayerState(a_Player)
+	local World = a_Player:GetWorld()
+	State.UndoStack:PushUndoFromCuboid(World, Cuboid, "drain")
+	
+	-- Process the area around the player using a cBlockArea:
 	local BlockArea = cBlockArea()
-	BlockArea:Read(Player:GetWorld(), MinX, MaxX, MinY, MaxY, MinZ, MaxZ) -- read the area
-	for x=0, BlockArea:GetSizeX() - 1 do
-		for y=0, BlockArea:GetSizeY() - 1 do
-			for z=0, BlockArea:GetSizeZ() - 1 do
-				if BlockArea:GetRelBlockType(x, y, z) == E_BLOCK_WATER or BlockArea:GetRelBlockType(x, y, z) == E_BLOCK_STATIONARY_WATER then -- check if the block is water
+	BlockArea:Read(World, Cuboid)
+	local NumBlocks = 0
+	for y = 0, BlockArea:GetSizeY() - 1 do
+		for z = 0, BlockArea:GetSizeZ() - 1 do
+			for x = 0, BlockArea:GetSizeX() - 1 do
+				local BlockType = BlockArea:GetRelBlockType(x, y, z)
+				if ((BlockType == E_BLOCK_WATER) or (BlockType == E_BLOCK_STATIONARY_WATER)) then
 					BlockArea:SetRelBlockType(x, y, z, 0) -- set the block to air
+					NumBlocks = NumBlocks + 1
 				end
 			end
 		end
 	end
-	BlockArea:Write(Player:GetWorld(), MinX, MinY, MinZ) -- write the are into the world.
+	BlockArea:Write(World, Cuboid.p1)
+	a_Player:SendMessage(cChatColor.LightPurple .. NumBlocks .. " block(s) changed.")
 	return true
 end
+
+
+
 
 
 ------------------------------------------------
@@ -468,69 +483,71 @@ function HandlePumpkinsCommand(Split, Player)
 end
 
 
-------------------------------------------------
----------------------SPHERE---------------------
-------------------------------------------------
-function HandleSphereCommand(Split, Player)
-	if Split[2] == nil or Split[3] == nil then
-		Player:SendMessage(cChatColor.Rose .. "Not enough parameters.")
-		Player:SendMessage(cChatColor.Rose .. "Usage: //sphere [BlockID] [Radius]")
-		return true
-	end
-	
-	local BlockType, BlockMeta = GetBlockTypeMeta(Player, Split[2])
 
-	if not BlockType then
-		Player:SendMessage(cChatColor.Rose .. "Unknown parameter \"" .. Split[2] .. "\"")
+
+
+function HandleSphereCommand(a_Split, a_Player)
+	-- //sphere <BlockType> <Radius>
+	
+	-- Check the params:
+	if ((a_Split[2] == nil) or (a_Split[3] == nil)) then
+		a_Player:SendMessage(cChatColor.Rose .. "Usage: //sphere <BlockType> <Radius>")
 		return true
 	end
 	
-	local Radius = tonumber(Split[3])
-	if not Radius then
-		Player:SendMessage(cChatColor.Rose .. "Unknown parameter \"" .. Split[3] .. "\"")
+	-- Convert the blocktype param:
+	local BlockType, BlockMeta = GetBlockTypeMeta(a_Player, a_Split[2])
+	if not(BlockType) then
+		a_Player:SendMessage(cChatColor.Rose .. "Unknown block type: \"" .. a_Split[2] .. "\"")
 		return true
 	end
 	
-	local World = Player:GetWorld()
-	local PosX = math.floor(Player:GetPosX())
-	local PosY = math.floor(Player:GetPosY())
-	local PosZ = math.floor(Player:GetPosZ())
-	
-	if PosY + Radius > 256 then
-		Player:SendMessage(cChatColor.Rose .. "You are at the top of the world. You can't build here.")
+	-- Convert the Radius param:
+	local Radius = tonumber(a_Split[3])
+	if not(Radius) then
+		a_Player:SendMessage(cChatColor.Rose .. "Cannot convert radius \"" .. a_Split[3] .. "\" to a number.")
 		return true
 	end
 	
+	-- Check if other plugins agree with the operation:
+	local World = a_Player:GetWorld()
+	local PosX = math.floor(a_Player:GetPosX())
+	local PosY = math.floor(a_Player:GetPosY())
+	local PosZ = math.floor(a_Player:GetPosZ())
 	local MinX, MaxX, MinY, MaxY, MinZ, MaxZ = PosX - Radius, PosX + Radius, PosY - Radius, PosY + Radius, PosZ - Radius, PosZ + Radius
-	
-	if CheckIfInsideAreas(MinX, MaxX, MinY, MaxY, MinZ, MaxZ, Player, World, "sphere") then
+	local Cuboid = cCuboid(MinX, MinY, MinZ, MaxX, MaxY, MaxZ)
+	Cuboid:ClampY(0, 255)
+	if not(CheckAreaCallbacks(Cuboid, a_Player, World, "sphere")) then
 		return true
 	end
 	
-	LastCoords[Player:GetName()] = {X = MinX, Y = MinY, Z = MinZ, WorldName = World:GetName()}
-	PersonalUndo[Player:GetName()]:Read(World, MinX, MaxX, MinY, MaxY, MinZ, MaxZ)
+	-- Push the area into an undo stack:
+	local State = GetPlayerState(a_Player)
+	State.UndoStack:PushUndoFromCuboid(World, Cuboid)
 	
+	-- Read the current contents of the world:
 	local BlockArea = cBlockArea()
-	BlockArea:Read(World, MinX, MaxX, MinY, MaxY, MinZ, MaxZ, 3)
-	local BlockAreaRadius = BlockArea:GetSizeX() - 1 -- All sides are the same size so we can use the GetSizeX function
-	
-	local MidPoint = Vector3d(BlockAreaRadius / 2, BlockAreaRadius / 2, BlockAreaRadius / 2)
-	
-	local Blocks = 0
-	for X=0, BlockAreaRadius do
-		for Y=0, BlockAreaRadius do
-			for Z=0, BlockAreaRadius do
-				local Distance = math.floor((MidPoint - Vector3d(X, Y, Z)):Length())
-				if Distance <= Radius then
+	BlockArea:Read(World, Cuboid, cBlockArea.baTypes + cBlockArea.baMetas)
+
+	-- Change blocks inside the sphere:
+	local MidPoint = Vector3d(Radius, PosY - MinY, Radius)  -- Midpoint of the sphere, relative to the area
+	local NumBlocks = 0
+	local SqrRadius = Radius * Radius
+	for Y = 0, Cuboid.p2.y - Cuboid.p1.y do  -- The Cuboid has been Y-clamped correctly, take advantage of that
+		for Z = 0, 2 * Radius do
+			for X = 0, 2 * Radius do
+				local Distance = math.floor((MidPoint - Vector3d(X, Y, Z)):SqrLength())
+				if (Distance <= SqrRadius) then
 					BlockArea:SetRelBlockTypeMeta(X, Y, Z, BlockType, BlockMeta)
-					Blocks = Blocks + 1
+					NumBlocks = NumBlocks + 1
 				end
 			end
 		end
 	end
 
+	-- Write the area back to world:
 	BlockArea:Write(World, MinX, MinY, MinZ)
-	Player:SendMessage(cChatColor.LightPurple .. Blocks .. " block(s) were created.")
+	a_Player:SendMessage(cChatColor.LightPurple .. NumBlocks .. " block(s) were affected.")
 	return true
 end
 
