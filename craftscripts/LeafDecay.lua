41301a3d0366c3fd64d6152d5390b907cd9e45a0
STRWarrior <niels.breuker@hotmail.nl>
2014-06-07 17:57:37 +0200

    Fixed leaves not being marked as decaying renamed ProcessLeave to ProcessLeaf

--- craftscripts/LeafDecay.lua
+++ craftscripts/LeafDecay.lua
@@ -39,10 +39,12 @@ BA2:CopyFrom(BA)
 local SizeX, SizeY, SizeZ = BA:GetSize()
 SizeX, SizeY, SizeZ = SizeX - 1, SizeY - 1, SizeZ - 1
 
-local function ProcessLeave(a_X, a_Y, a_Z, a_I)
+local function ProcessLeaf(a_X, a_Y, a_Z, a_I)
 	local BlockType = BA:GetRelBlockType(a_X, a_Y, a_Z)
 	if ((BlockType == E_BLOCK_LEAVES) or (BlockType == E_BLOCK_NEW_LEAVES)) then
 		BA:SetRelBlockType(a_X, a_Y, a_Z, E_BLOCK_SPONGE)
+	else
+		return
 	end
 	
 	local I = a_I - 1
@@ -50,12 +52,12 @@ local function ProcessLeave(a_X, a_Y, a_Z, a_I)
 		return
 	end
 	
-	ProcessLeave(a_X - 1, a_Y, a_Z, I)
-	ProcessLeave(a_X + 1, a_Y, a_Z, I)
-	ProcessLeave(a_X, a_Y - 1, a_Z, I)
-	ProcessLeave(a_X, a_Y + 1, a_Z, I)
-	ProcessLeave(a_X, a_Y, a_Z - 1, I)
-	ProcessLeave(a_X, a_Y, a_Z + 1, I)
+	ProcessLeaf(a_X - 1, a_Y, a_Z, I)
+	ProcessLeaf(a_X + 1, a_Y, a_Z, I)
+	ProcessLeaf(a_X, a_Y - 1, a_Z, I)
+	ProcessLeaf(a_X, a_Y + 1, a_Z, I)
+	ProcessLeaf(a_X, a_Y, a_Z - 1, I)
+	ProcessLeaf(a_X, a_Y, a_Z + 1, I)
 end
 	
 
@@ -64,12 +66,12 @@ for X = 0, SizeX do
 		for Z = 0, SizeZ do
 			local BlockType = BA:GetRelBlockType(X, Y, Z)
 			if ((BlockType == E_BLOCK_LOG) or (BlockType == E_BLOCK_NEW_LOG)) then
-				ProcessLeave(X - 1, Y, Z, LEAVES_CHECK_DISTANCE)
-				ProcessLeave(X + 1, Y, Z, LEAVES_CHECK_DISTANCE)
-				ProcessLeave(X, Y - 1, Z, LEAVES_CHECK_DISTANCE)
-				ProcessLeave(X, Y + 1, Z, LEAVES_CHECK_DISTANCE)
-				ProcessLeave(X, Y, Z - 1, LEAVES_CHECK_DISTANCE)
-				ProcessLeave(X, Y, Z + 1, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X - 1, Y, Z, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X + 1, Y, Z, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X, Y - 1, Z, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X, Y + 1, Z, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X, Y, Z - 1, LEAVES_CHECK_DISTANCE)
+				ProcessLeaf(X, Y, Z + 1, LEAVES_CHECK_DISTANCE)
 			end
 		end
 	end
