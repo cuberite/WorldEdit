eb587bc471c5bc9a5da6b65d500e4a29ee14c91d
STRWarrior <niels.breuker@hotmail.nl>
2014-08-01 00:13:17 +0200

    Updated /remove Now works dynamicly, and multiple entity types can be removed.

--- cmd_Entities.lua
+++ cmd_Entities.lua
@@ -1,33 +1,73 @@
+
+-- cmd_Entities.lua
+
+-- Contains commands that do things with entities.
+
+
+
+
+
 ------------------------------------------------
 ---------------------REMOVE---------------------
 ------------------------------------------------
-function HandleRemoveCommand(Split, Player)
-	if Split[2] == nil then -- check if the player gave a radius
-		Player:SendMessage(cChatColor.Rose .. "Too few arguments.\n/remove <type>")
+function HandleRemoveCommand(a_Split, a_Player)
+	-- /remove <EntityTypes>
+	
+	-- Collect all the entity names with their corresponding type
+	local Types = {}
+	for Key, Value in pairs(cEntity) do
+		if ((type(Value) == "number") and (Key:sub(1, 2) == "et") and (Value ~= cEntity.etPlayer)) then
+			Types[Key:sub(3, Key:len()):lower()] = Value
+		end
+	end
+	
+	-- Check if the player gave a parameter. If not show them a list of acceptable types.
+	if (a_Split[2] == nil) then
+		a_Player:SendMessage(cChatColor.Rose .. "Too few arguments.\n/remove <type>")
+		local ListEntityNames = ""
+		for EntityName, _ in pairs(Types) do
+			ListEntityNames = ListEntityNames .. EntityName .. ", "
+		end
+		
+		a_Player:SendMessage(cChatColor.Rose .. "Acceptable types: " .. ListEntityNames:sub(1, ListEntityNames:len() - 2))
 		return true
 	end
-	local Entitys = 0
-	if string.upper(Split[2]) == "ITEMS" then -- check if the plugin has to destroy pickups
-		local LoopEntity = function(Entity)
-			if Entity:IsPickup() then -- if the entity is a pickup then destroy it.
-				Entity:Destroy()
-				Entitys = Entitys + 1
-			end
+	
+	-- Used to check if there is at least one entity type to be removed.
+	local NumEntityTypes = 0
+	
+	-- Check if the parameters given are valid. If not, then ignore them.
+	local EntityTypes = {}
+	local EntityNames = StringSplit(a_Split[2], ",")
+	for Idx, EntityName in ipairs(EntityNames) do
+		local LowerCasedEntityName = EntityName:lower()
+		if (Types[LowerCasedEntityName]) then
+			EntityTypes[Types[LowerCasedEntityName]] = true
+			NumEntityTypes = NumEntityTypes + 1
+		else
+			a_Player:SendMessage(cChatColor.Rose .. "Unknown entity \"" .. EntityName .. "\". Ignoring it.")
 		end
-		Player:GetWorld():ForEachEntity(LoopEntity)
-	elseif string.upper(Split[2]) == "MINECARTS" then -- check if the plugin needs to destroy minecarts
-		local LoopEntity = function(Entity)
-			if Entity:IsMinecart() then -- if the entity is a minecart then destroy it 
-				Entity:Destroy()
-				Entitys = Entitys + 1
-			end
-		end
-		Player:GetWorld():ForEachEntity(LoopEntity)	
-	else
-		Player:SendMessage(cChatColor.Rose .. "Acceptable types: items, minecarts") -- the entity that the player wants to destroy does not exist.
+	end
+	
+	-- Bail out of not even one entity type will be removed.
+	if (NumEntityTypes == 0) then
 		return true
 	end
-	Player:SendMessage(cChatColor.LightPurple .. "Marked " .. Entitys .. " entit(ies) for removal.")
+	
+	local NumRemovedEntities = 0
+	
+	-- Go through every entity and check if it should be removed
+	a_Player:GetWorld():ForEachEntity(
+		function(a_Entity)
+			if (EntityTypes[a_Entity:GetEntityType()]) then
+				a_Entity:Destroy()
+				NumRemovedEntities = NumRemovedEntities + 1
+			end
+		end
+	)
+	
+	-- Send a message to the player.
+	a_Player:SendMessage(cChatColor.LightPurple .. "Marked " .. NumRemovedEntities .. " entit(ies) for removal.")
 	return true
 end
 
