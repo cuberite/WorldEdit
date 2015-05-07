5c42235f7fd3023ca5a275717c5a696316d87c97
STRWarrior <niels.breuker@hotmail.nl>
2015-05-07 21:52:39 +0200

    Added documentation to cBlockSrc

--- Classes/BlockSrc.lua
+++ Classes/BlockSrc.lua
@@ -1,4 +1,8 @@
 
+-- BlockSrc.lua
+
+-- Contains the BlockSrc class representing blocks that can be replaced. Used for example in masks and the replace command.
+
 
 
 
@@ -10,13 +14,16 @@ cBlockSrc = {}
 
 
 function cBlockSrc:new(a_Blocks)
+	-- Get all the different blocks from the string
 	local BlockArray, ErrorBlock = RetrieveBlockTypes(a_Blocks)
 	if (not BlockArray) then
 		return false, ErrorBlock
 	end
 	
+	-- Make from the array a table with the blocktypes as keys.
+	-- In there create a table a boolean value called TypeOnly, and a table containing blockmetas
+	-- If TypeOnly is set to true the Contains function will only check if the blocktype exists. Else it will look if the blockmeta exists in the BlockMetas table.
 	local BlockTable = {}
-	
 	for Idx, Block in ipairs(BlockArray) do
 		local BlockInfo = BlockTable[Block.BlockType] or {TypeOnly = false, BlockMetas = {}}
 		BlockInfo.TypeOnly = BlockInfo.TypeOnly or Block.TypeOnly
@@ -24,6 +31,7 @@ function cBlockSrc:new(a_Blocks)
 			BlockInfo.BlockMetas[Block.BlockMeta] = true
 		end
 		
+		-- Save the blockinfo in the table with the blocktype as key.
 		BlockTable[Block.BlockType] = BlockInfo
 	end
 	
@@ -41,6 +49,7 @@ end
 
 
 
+-- Checks if the given blocktype exists in the blocktable.
 function cBlockSrc:Contains(a_BlockType, a_BlockMeta)
 	local BlockInfo = self.m_BlockTable[a_BlockType]
 	if (not BlockInfo) then
@@ -48,9 +57,12 @@ function cBlockSrc:Contains(a_BlockType, a_BlockMeta)
 	end
 	
 	if (BlockInfo.TypeOnly) then
+		-- The block is marked to only check the blocktype, so we don't have to check the meta.
+		-- Since the block exists in the blocktable we can just return true
 		return true
 	end
 	
+	-- Check if the meta exists. If it exists it has the value true, so we either return that or return false.
 	return BlockInfo.BlockMetas[a_BlockMeta] or false
 end
 
