50385e65ac3aac8e756241c6bf96ce0dbd074963
Lukas Pioch <lukas@zgow.de>
2015-12-12 09:37:53 +0100

    Changed Split to a_Split

--- Commands/Entities.lua
+++ Commands/Entities.lua
@@ -81,7 +81,7 @@ function HandleButcherCommand(a_Split, a_Player)
 	if (a_Split[2] == nil) then -- if the player did not give a radius then the radius is the normal radius
 		Radius = g_Config.Defaults.ButcherRadius
 	elseif (tonumber(a_Split[2]) == nil) then -- if the player gave a string as radius then stop
-		a_Player:SendMessage(cChatColor.Rose .. 'Number expected; string "' .. Split[2] .. '" given')
+		a_Player:SendMessage(cChatColor.Rose .. 'Number expected; string "' .. a_Split[2] .. '" given')
 		return true
 	else -- the radius is set to the given radius
 		Radius = tonumber(a_Split[2])
